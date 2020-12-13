5c7faef3c1039fb70a2b2bbf09aeb9f60b23aed3
ramishenouda <ramishenouda@outlook.com>
2020-12-13 02:57:04 +0200

    updated the logic

--- scripts.js
+++ scripts.js
@@ -2,34 +2,13 @@ const intro = document.querySelector('.intro');
 const video = intro.querySelector('video');
 const text = intro.querySelector('h1');
 
-// section 2
 const section = document.querySelector('section');
 const end = section.querySelector('h1');
 
-// ScrollMagic
-const controller = new ScrollMagic.Controller();
+const FRAMES = 148;
+const FPS = 60;
 
-// Scenes
-let scene = new ScrollMagic.Scene({
-    duration: 4660,
-    triggerElement: intro,
-    triggerHook: 0
-})
-.setPin(intro)
-.addTo(controller);
-
-
-// Video animation
-let accelAmount = 0.1;
-let scrollPos = 0;
-let delay = 0;
-let reversing = false;
-
-scene.on("update", e => {
-    scrollPos = e.scrollPos / 1000;
+window.addEventListener('scroll', function (e) {
+    var time = (window.scrollY / 1000) * FRAMES / FPS;
+    video.currentTime = time;
 });
-
-setInterval(() => {
-    delay += (scrollPos - delay) * accelAmount;
-    video.currentTime = delay;
-}, 60);
